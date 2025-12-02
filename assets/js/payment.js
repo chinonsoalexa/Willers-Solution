3e7e1fe56b9a21dbe9a29d830247f7f0f423e40b
Chinonso Alexander <chinonsoalexander2@gmail.com>
2025-12-02 07:47:02 +0100

    fix repo structure

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -1,14 +1,10 @@
 async function loadTransactionSummary() {
     // Get bookingID from the URL
     const bookingID = new URLSearchParams(window.location.search).get("OrderID");
-    var orderIdLoc;
+    var orderIdLoc = localStorage.getItem("OrderID");
 
-    if (!bookingID) {
-        orderIdLoc = localStorage.getItem("OrderID");
-
-        const newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?OrderID=' + orderIdLoc;
-        history.pushState({ path: newUrl }, '', newUrl);
-    }
+    const newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?OrderID=' + orderIdLoc;
+    history.pushState({ path: newUrl }, '', newUrl);
 
     try {
         // Make the request
